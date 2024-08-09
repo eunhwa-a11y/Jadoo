4b8582ac7d2b5e38ad283906bdda6e1f3008bd61
eunhwa-a11y <ttab136@gmail.com>
2024-08-09 13:12:33 +0900

    Partners js 완료

--- js/script.js
+++ js/script.js
@@ -5,6 +5,39 @@ let paginationDown = document.querySelector('.pagination .down');
 let currentIdx = 0;
 let testimonialCount = testimonialsLists.length;
 
+let partnerList = document.querySelector('.partner_list');
+let partnerListWidth = 234;
+let partnerListCount = document.querySelectorAll('.partner_list li').length;
+let partnerListLeft = 0;
+let partnerListTotalWidth = partnerListWidth * partnerListCount;
+let animation;
+
+
+partnerList.style.width = partnerListTotalWidth + 'px';
+
+function MovePartnerList(){
+
+  // partnerListLeft = partnerListLeft - 5;
+  partnerListLeft -= 2;
+
+  if(partnerListLeft === -(partnerListTotalWidth/2)){
+    partnerListLeft = 0;
+  }
+
+  partnerList.style.left = partnerListLeft + 'px';
+  animation = requestAnimationFrame(MovePartnerList);
+
+}
+requestAnimationFrame(MovePartnerList);
+
+partnerList.addEventListener('mouseenter', () => {
+  cancelAnimationFrame(animation);
+})
+
+partnerList.addEventListener('mouseleave', () => {
+  requestAnimationFrame(MovePartnerList);
+})
+
 
 /*
 pagers를 클릭하면 할일
