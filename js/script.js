a295a84685283bf416cd6a405947f6fea690f5dc
eunhwa-a11y <ttab136@gmail.com>
2024-08-09 12:02:57 +0900

    Testimonial function add

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let pagers = document.querySelectorAll('.testimonials .pager a');
-let testimonialsLists = document.querySelectorAll('.testimonials_list li')
-let paginationUp = document.querySelector('pagination .up');
-let paginationDoun = document.querySelector('pagination .down');
+let testimonialsLists = document.querySelectorAll('.testimonials_list li');
+let paginationUp = document.querySelector('.pagination .up');
+let paginationDown = document.querySelector('.pagination .down');
+let currentIdx = 0;
+let testimonialCount = testimonialsLists.length;
 
 
 /*
@@ -9,13 +11,60 @@ pagers를 클릭하면 할일
   모든 a에서 active를 제거하고, 클릭한 그 요소에 active 추가
 */
 
-pagers.forEach((item, idx) => {
-  item.addEventListener('click', (e) =>{
+pagers.forEach((item,idx)=>{
+
+  item.addEventListener('click',(e)=>{
     e.preventDefault();
 
-    for(let pager of pagers){
-      pager.classList.remove('active');
-    };
-    item.classList.add('active');
+    showTestimonial(idx);
+
   });
+
+});
+
+function showTestimonial(num){
+
+  /*
+  if(num === -1){
+    num = testimonialCount -1;
+  } else if(num === 3){
+    num = 0;
+  } else {
+    num = num;
+  }
+  num = (num === -1) ? testimonialCount -1 : (num === 3) ? 0:num;
+  */
+  
+  num = (num + testimonialCount) % testimonialCount;
+
+  /*
+  num = (1+3) % 3 = 1
+  num1 num1
+
+  num = (2+3) % 3 = 2
+  num2 num2
+
+  num = (3+3) % 3 = 0
+  num3 num0
+  */
+
+  for(let testimonial of testimonialsLists){
+    testimonial.classList.remove('active');
+  }
+  testimonialsLists[num].classList.add('active');
+  currentIdx = num;
+
+  for(let pager of pagers){
+    pager.classList.remove('active');
+  }
+  pagers[num].classList.add('active');
+
+}
+
+paginationDown.addEventListener('click',()=>{
+  showTestimonial(currentIdx + 1);
+});
+
+paginationUp.addEventListener('click',()=>{
+  showTestimonial(currentIdx - 1);
 });
